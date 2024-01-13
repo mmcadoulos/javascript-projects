5ec2c85235427734a577f20939ffa0799c33b1da
Μιχαίας <mmcadoulos@gmail.com>
2024-01-13 17:27:11 -0600

    worked through it and enjoyed the mad lib

--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -1,4 +1,18 @@
-let jsCreator = "Brendan Eich";
+// let jsCreator = "Brendan Eich";
 
-console.log(jsCreator[-1]);
-console.log(jsCreator[42]);
+// console.log(jsCreator[-1]);
+// console.log(jsCreator[42]);
+
+// let phrase = "JavaScript rocks!";
+// console.log(phrase[phrase.length - 8]);
+// console.log(phrase.length);
+
+// let pet = "cat";
+//     console.log(pet.replace("c", 'Q'));
+
+// cityName = "Vienna";
+// stateName = "Virginia";
+// location = cityName + ", " +  stateName;
+
+// console.log(location.length);
+
--- stringing-characters-together/code-snippets/method-chaining.js
+++ stringing-characters-together/code-snippets/method-chaining.js
@@ -1,11 +1,25 @@
 //String methods can be combined in a process called method chaining.
 
-let word = 'JavaScript';
+// let word = 'JavaScript';
 
-console.log(word.toUpperCase());
+// console.log(word.toUpperCase());
 //Returns ``JAVASCRIPT``
 
 //What does ``word.slice(4).toUpperCase()`` return?
 
+// let word = 'JavaScript';
+// console.log(word.slice(4).toUpperCase());
+// returns ``SCRIPT``
 
 //Experiment with other combinations (chains) of string methods.
+
+// let language = "JavaScript";
+// language.replace('J', 'Q');
+// language.slice(0,5);
+// console.log(language);
+// returns ``JavaScript``
+
+let org = "  The LaunchCode Foundation ";
+let trimmed = org.trim();
+
+console.log(trimmed);
