a400ebfd87c0b7abd99dc5d5585ff3b35e7fa173
Μιχαίας <mmcadoulos@gmail.com>
2024-01-16 22:13:44 -0600

    began loops chapter

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,10 @@
 // create an array variable containing the names
-
+let familyNames = ["Micah", "Ruth", "Moses"];
 // write a for loop that prints each name on a different line
+for (let i = 0; i < familyNames.length; i++){
+    console.log(familyNames[i]);
+}
+console.log("so far....");
+for (let j = familyNames.length - 1; j > -1; j--){
+    console.log(familyNames[j]);
+}
