64fc25cc03b1de31fdaa1a0d7d04f3bc291f8a82
Μιχαίας <mmcadoulos@gmail.com>
2024-01-20 10:08:17 -0600

    Loops practice

--- loops/chapter-examples/Loop-Variable.js
+++ loops/chapter-examples/Loop-Variable.js
@@ -1,5 +1,7 @@
 // Experiment with this loop by modifying each of the following: the variable initialization, the boolean condition, and the update expression.
 
-for (let i = 0; i < 51; i++) {
-   console.log(i);
- }
+let array = ["banana", "monkey", "The Monkees"];
+let answer = "" 
+for (let i = 0; i <= 2; i++) {
+   console.log(array[i]);
+ };
--- loops/chapter-examples/while-Loop-Example.js
+++ loops/chapter-examples/while-Loop-Example.js
@@ -1,6 +1,46 @@
+// let i = 0;
+
+// while (i < 51) {
+//   console.log(i);
+//   i++;
+// }
+
+// const input = require('readline-sync');
+
+// let num = input.question('Please enter a positive number:');
+// num = Number(num);
+
+// while (num <= 0) {
+//    num = input.question('Invalid input. Please enter a positive number:');
+//    num = Number(num);
+// }
+
+// let i = 100;
+
+// while (i > 50) {
+//    console.log(i);
+//    i++;
+//    if (i == 353){
+//     console.log(`Overflow of ${i} reached! ABORT`);
+//     break;
+//    }
+// }
+const input = require('readline-sync');
+let numbers = [1, 25, 83, 492,283, "BANANA", 983];
+let searchVal = input.question("What are ye searchin' fer in this here array?: ");
 let i = 0;
 
-while (i < 51) {
-  console.log(i);
-  i++;
+while (i < numbers.length) {
+   if (numbers[i] === Number(searchVal) || String(numbers[i]).toLowerCase() === searchVal.toLowerCase()) {
+      break;
+   }
+   i++;
 }
+console.log(i);
+if (i < numbers.length) {
+   console.log("The value", searchVal, "was located at index", i);
+} else {
+   console.log("The value", searchVal, "is not in the array.");
+}
+
+
