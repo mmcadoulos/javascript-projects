0a3812047ea6ef57f95738a1d87892109605e930
Μιχαίας <mmcadoulos@gmail.com>
2024-01-27 16:05:10 -0600

    confused with the raid part of exercise but I think I did it?

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -1,5 +1,13 @@
 //Create an anonymous function and set it equal to a variable.
-
+let inputMorph = function (input){
+    if (typeof(input) === "number"){
+        return input * 3;
+    } else if (typeof(input) === "string"){
+        return "ARRR!";
+    }
+    return input;
+}
+console.log(inputMorph(99));
 /* Your function should:
 a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
@@ -11,5 +19,14 @@ a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
 c) Print the new array to confirm your work.
 */
-
+// let inputMorph = function (input){
+//     if (typeof(input) === "number"){
+//         return input * 3;
+//     } else if (typeof(input) === "string"){
+//         return "ARRR!";
+//     }
+//     return input;
+// }
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+let mappedArray = arr.map(inputMorph);
+console.log(mappedArray);
--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -18,15 +18,44 @@ function holdStatus(arr){
   }
 }
 
+let monkeyStatus = function(bananaBunches){
+  if (checkFuel(bananaBunches) === "green"){
+    return bananaBunches - 100001;
+  } 
+  else if (checkFuel(bananaBunches) === "yellow"){
+    return bananaBunches - 50001;
+  } 
+  else {
+    return bananaBunches;
+  }
+};
+
+let captainDiary = function(array){
+  let loot = [];
+  // loot.push(array.slice(6), array.slice(1,2));
+  // array.push("paper straws", "teddy bear");
+  loot.push(array[6], array[1]);
+  array.splice(1, 1, "straws");
+  array.splice(6, 1, "teddy bears");
+  return loot;
+};
+
+let irs = function(fuelLevel, itemsInCargo){
+  let loot = captainDiary(itemsInCargo);
+  return `Raided ${monkeyStatus(fuelLevel)} kg of fuel from the tanks, and stole ${loot[0]} and ${loot[1]} from the cargo hold.`
+};
+
 let fuelLevel = 200000;
 let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold', 'water', 'AE-35 unit'];
-
+// fuelLevel = fuelLevel - monkeyStatus(fuelLevel);
+// captainDiary(cargoHold);
+console.log(irs(fuelLevel, cargoHold));
 console.log("Fuel level: " + checkFuel(fuelLevel));
 console.log("Hold status: " + holdStatus(cargoHold));
 
 /* Steal some fuel from the shuttle:
- * /
- 
+*/
+
 //a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
 
 //b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
@@ -36,7 +65,7 @@ console.log("Hold status: " + holdStatus(cargoHold));
 //d). Decide where to best place your function call to gather our new fuel.
 
 /* Next, liberate some of that glorious cargo.
- * /
+ */
 
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
 
@@ -47,7 +76,7 @@ console.log("Hold status: " + holdStatus(cargoHold));
 //d). Don’t get hasty, matey! Remember to test your function.
 
 /* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
- * /
+ */
  
 //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
 	
