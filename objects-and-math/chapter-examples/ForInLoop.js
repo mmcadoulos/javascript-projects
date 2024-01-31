6c3edc665f1d376deea48234177bfa9ad88efe03
Μιχαίας <mmcadoulos@gmail.com>
2024-01-30 20:42:17 -0600

    finished reading

--- objects-and-math/chapter-examples/ForInLoop.js
+++ objects-and-math/chapter-examples/ForInLoop.js
@@ -1,3 +1,16 @@
+// let giraffe = {
+//    species: "Reticulated Giraffe",
+//    name: "Cynthia",
+//    weight: 1500,
+//    age: 15,
+//    diet: "leaves"
+//    ,birthday: function () {
+//       return this.age = this.age + 1;
+//    }
+//  };
+
+//  console.log(giraffe.birthday());
+
 let tortoiseOne = {
    species: "Galapagos Tortoise",
    name: "Pete",
@@ -7,3 +20,10 @@ let tortoiseOne = {
 };
 
 // Using a for..in loop, iterate through each property in the tortoiseOne object and print the value to the console.
+//  for (banana in tortoiseOne){
+//    console.log(banana + ": " + tortoiseOne[banana]);
+//  }
+
+console.log(Math.round(-3.87));
+
+
