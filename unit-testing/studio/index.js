3592e17d4990d2d80a9a8250d4438554a668c510
Μιχαίας <mmcadoulos@gmail.com>
2024-02-08 20:19:46 -0600

    finished studio, no bonus

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,32 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageOfCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: (num)=>{
+    let output = "";
+    if (num%2 === 0 || num%3 === 0 || num%5 === 0){
+        if (num%2 === 0 && num%3 !== 0 && num%5 !== 0){
+            output = 'Launch!';
+        } else if (num%3 === 0 && num%2 !== 0 && num%5 !== 0){
+            output = 'Code!';
+        } else if (num%5 === 0 && num%2 !== 0 && num%3 !== 0){
+            output = 'Rocks!';
+        } else if (num%2 === 0 && num%3 === 0 && num%5 !== 0){
+            output = 'LaunchCode!';
+        } else if (num%3 === 0 && num%5 === 0 && num%2 !== 0){
+            output = 'Code Rocks!';
+        } else if (num%2 === 0 && num%5 === 0 && num%3 !== 0){
+            output = 'Launch Rocks! (CRASH!!!!)';
+        } else if (num%2 === 0 && num%3 === 0 && num%5 === 0){
+            output = 'LaunchCode Rocks!';
+        }
+     } else {
+        output = "Rutabagas! That doesn't work.";
+     }
+     return output;
+    }
 }
 
 module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,35 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test("testing organization", ()=>{
+    console.log(launchcode.organization);
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+
+  test("testing executiveDirector", ()=>{
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+
+  test("testing percentageOfCoolEmployees",()=>{
+    expect(launchcode.percentageOfCoolEmployees).toBe(100);
+  });
+
+  test("testing programsOffered", ()=>{
+    //console.log(launchcode.programsOffered);
+    expect(launchcode.programsOffered).toEqual(["Web Development", "Data Analysis", "Liftoff"]);
+  });
+    expect(launchcode.programsOffered.length).toBe(3);
+
+  test("testing launchOutput()", ()=>{
+    expect(typeof launchcode.launchOutput()).toBe("string");
+    expect(launchcode.launchOutput(4)).toBe("Launch!");
+    expect(launchcode.launchOutput(9)).toBe('Code!');
+    expect(launchcode.launchOutput(25)).toBe('Rocks!');
+    expect(launchcode.launchOutput(91)).toBe("Rutabagas! That doesn't work.");
+    expect(launchcode.launchOutput(6)).toBe('LaunchCode!');
+    expect(launchcode.launchOutput(15)).toBe('Code Rocks!');
+    expect(launchcode.launchOutput(10)).toBe('Launch Rocks! (CRASH!!!!)');
+    expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!');
+  });
+
 });
