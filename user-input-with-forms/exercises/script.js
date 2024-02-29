efc683cbb67572a11e99916d8cf1992270042427
Μιχαίας <mmcadoulos@gmail.com>
2024-02-28 22:28:36 -0600

    couldn't finish the bonus mission. Radio and Select validation confusing

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,28 @@
 //Code Your Solution Below
+function init() {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    let nameInput = document.querySelector("input[name=testName]");
+    let dateInput = document.querySelector("input[name=testDate]");
+    let rocketTypeInput = document.querySelector("input[name=rocketType]");
+    let boosterInput = document.querySelector("input[name=boosterCount]");
+    let windInput = document.querySelector("input[name=windRating]");
+    let serversInput = document.querySelector("input[name=productionServers]");
+    console.log(windInput.value)
+    if (
+      nameInput.value === "" ||
+      dateInput.value === "" ||
+      /*rocketTypeInput.value === '' ||*/
+      boosterInput.value === '' ||
+      windInput.value != 25||
+      serversInput.value === ""
+    ) {
+      alert("All fields are required!!");
+      event.preventDefault();
+    }
+
+    console.log("all systems go!");
+  });
+}
+
+window.addEventListener("load", init);
